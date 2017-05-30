8f781c9ae3242a637ad22189e80d3f0e5c685730
User <User@CLZ7J32>
2017-05-30 14:52:52 +0200

    Add project files.

---
+++ GitTest/Ui.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Ui
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello Git");
+        }
+    }
+}
